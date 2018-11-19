09dbb226316fee029d98be792c686a3123b42ba9
Matt <42591412+ellis-matthew4@users.noreply.github.com>
2018-11-19 18:47:53 -0500

    Added a few rooms

--- assets/scripts/room.gd
+++ assets/scripts/room.gd
@@ -1,8 +1,27 @@
-extends Navigation2D
+extends TileMap
 
-onready var priest = get_tree().get_nodes_in_group("priest")[0]
-
-signal Switch
+export(Vector2) var BoxelPosition
+export(Vector2) var BoxelSize
+export(bool) var Top
+export(bool) var Right
+export(bool) var Bottom
+export(bool) var Left
 
 func _ready():
-	priest.set_nav(self)
+	pass
+
+func rotate_clockwise():
+	rotation_degrees += 90
+	var old = [ Top, Right, Bottom, Left ]
+	Top = old[3]
+	Right = old[0]
+	Bottom = old[1]
+	Left = old[2]
+	
+func rotate_counterClockwise():
+	rotation_degrees -= 90
+	var old = [ Top, Right, Bottom, Left ]
+	Top = old[1]
+	Right = old[2]
+	Bottom = old[3]
+	Left = old[0]
