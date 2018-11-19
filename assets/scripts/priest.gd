cddcc8099c616637c6ebe38c9ce9f2ead10ea87c
Matt <42591412+ellis-matthew4@users.noreply.github.com>
2018-11-19 13:34:52 -0500

    Added Priest sprite

--- assets/scripts/priest.gd
+++ assets/scripts/priest.gd
@@ -26,38 +26,34 @@ func _ready():
 func _process(delta):
 	playerPoint = player.global_position
 	playerAngle = rad2deg(global_position.angle_to_point(playerPoint))
-	if playerAngle < -157.5 or playerAngle > 157.5:
-		$Sprite.play("right")
-	elif playerAngle > 112.5 and playerAngle <= 157.5:
-		$Sprite.play("upRight")
+	if (playerAngle < -157.5 or playerAngle > 157.5) or (playerAngle > -22.5 and playerAngle <= 22.5):
+		$Sprite.play("left")
+	elif (playerAngle > 112.5 and playerAngle <= 157.5) or (playerAngle > 22.5 and playerAngle <= 67.5):
+		$Sprite.play("upLeft")
 	elif playerAngle > 67.5 and playerAngle <= 112.5:
 		$Sprite.play("up")
-	elif playerAngle > 22.5 and playerAngle <= 67.5:
-		$Sprite.play("upLeft")
-	elif playerAngle > -22.5 and playerAngle <= 22.5:
-		$Sprite.play("left")
-	elif playerAngle < -22.5 and playerAngle >= -67.5:
-		$Sprite.play("downLeft")
 	elif playerAngle < -67.5 and playerAngle >= -112.5:
 		$Sprite.play("down")
 	else:
-		$Sprite.play("downRight")
+		$Sprite.play("downLeft")
+		
+	if playerAngle < 90 or playerAngle > -90:
+		$Sprite.flip_h = false
+	if playerAngle > 90 or playerAngle < -90:
+		$Sprite.flip_h = true
 		
 	var walk_distance = SPEED * delta
 	move_along_path(walk_distance)
 	
 func move_along_path(distance):
 	if position.distance_to(player.position) > 32:
+		$Sprite.playing = true
 		var last_point = position
 		for index in range(path.size()):
 			var distance_between_points = last_point.distance_to(path[0])
 			if distance <= distance_between_points and distance >= 0.0:
 				motion = last_point.linear_interpolate(path[0], distance / distance_between_points)
-				if test_motion(motion):
-					position = motion
-				else:
-					motion *= Vector2(-1,-1)
-					position = motion
+				position = motion
 				break
 			elif distance < 0.0:
 				position = path[0]
@@ -66,6 +62,7 @@ func move_along_path(distance):
 			last_point = path[0]
 			path.remove(0)
 	else:
+		$Sprite.playing = false
 		if position.distance_to(player.position) > 64:
 			snap()
 			
